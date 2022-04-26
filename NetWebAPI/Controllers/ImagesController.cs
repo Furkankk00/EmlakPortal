3835713a4e3c86475598c0284e60002e05825814
kucuk <kucuk@FURKAN>
2022-04-26 18:38:49 +0300

    Add project files.

---
+++ NetWebAPI/Controllers/ImagesController.cs
@@ -0,0 +1,79 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Entities.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageService _imageService;
+
+        public ImagesController(IImageService imageService)
+        {
+            _imageService = imageService;
+        }
+
+        [HttpPost("upload")]
+        public IActionResult Upload([FromForm] UserForImageDto userForImageDto)
+        {
+            var result = _imageService.Add(userForImageDto);
+            if (result.Status)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromBody] Image image)
+        {
+            var result = _imageService.Delete(image);
+            if (result.Status)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] Image image)
+        {
+            var result = _imageService.Update(image);
+            if (result.Status)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("get-all")]
+        public IActionResult GetAll()
+        {
+            var result = _imageService.GetAll();
+            if (result.Status)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("getby-image-id/{imageId}")]
+        public IActionResult GetByImageId(int imageId)
+        {
+            var result = _imageService.GetByImageId(imageId);
+            if (result.Status)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
